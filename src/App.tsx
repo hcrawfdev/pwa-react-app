124d6575573827b1d56f7ecc8024f3cd24a44d91
hcrawf <harrisoncrawf1@gmail.com>
2021-08-30 22:10:09 -0400

    Initial PWA commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-green-900">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="animate-spin-slow" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
